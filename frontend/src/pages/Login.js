602c7c9aa1c51f30cc74249c91e7557841201f01
Aarush Jain <aarushj09@gmail.com>
2023-05-05 16:44:14 +0530

    login/signup

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,15 +1,48 @@
 import React from "react";
 import { Link } from 'react-router-dom'
+import axios from "axios";
+import { useState } from "react";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChangeEmail = (e) => {
+      setEmail(e.target.value);
+    };
+    const onChangePassword = (e) => {
+      setPassword(e.target.value);
+    };
+    const resetInputs = () => {
+      setEmail("");
+      setPassword("");
+    };
+
+    const onSubmit = (e) => {
+      e.preventDefault();
+      const user = {
+        email:email,
+        password:password
+      };
+      axios
+        .post("", user)
+        .then((res) => {
+          console.log(res.data);
+          resetInputs();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
+
     return (
     <div className='auth'>
       <h1>Login</h1>
       <form>
-        <input required type="text" placeholder='Username' />
-        <input required type="password" placeholder='Password'/> 
-        <button>Login</button>
-        {/* <p> Error! </p> */}
+        <input required type="text" placeholder='Email' value={email} onChange={onChangeEmail} />
+        <input required type="password" placeholder='Password' value={password} onChange={onChangePassword}/> 
+        <button onClick={onSubmit}>Login</button>
         <span>Don't you have an account? <Link to="/signup">Register</Link></span>
       </form>
     </div>
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,18 +1,57 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+
 
 const Signup = () => {
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChangeUsername = (e) => {
+    setUsername(e.target.value);
+  };
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const resetInputs = () => {
+    setUsername("");
+    setEmail("");
+    setPassword("");
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const user = {
+      email:email,
+      password:password
+    };
+    axios
+      .post("", user)
+      .then((res) => {
+        console.log(res.data);
+        window.location="/login";
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    resetInputs();
+  };
   return (
     <div className="auth">
       <h1>Sign-up</h1>
       <form>
-        <input required type="text" placeholder="Username" />
-        <input required type="email" placeholder="Email ID" />
-        <input required type="password" placeholder="Password" />
-        <button>Login</button>
+        <input required type="text" placeholder="Username" value={username} onChange={onChangeUsername} />
+        <input required type="email" placeholder="Email ID" value={email} onChange={onChangeEmail}/>
+        <input required type="password" placeholder="Password" value={password} onChange={onChangePassword}/>
+        <button onClick={onSubmit}>Login</button>
         {/* <p> Error! </p> */}
         <span>
-          Do you have an account? <Link to="/login">Login</Link>
+          Already have an account? <Link to="/login">Login</Link>
         </span>
       </form>
     </div>
