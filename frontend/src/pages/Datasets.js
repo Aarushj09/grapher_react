04a5ebe61462bcefb6a27e61af95a96e90f4f9ca
Aarush Jain <aarushj09@gmail.com>
2023-05-08 01:05:41 +0530

    add dataset button

--- frontend/src/pages/Datasets.js
+++ frontend/src/pages/Datasets.js
@@ -15,11 +15,29 @@ const Datasets = () => {
 		}
 	]);
 
+	const changeHandler = (event) => {
+		datasets.push({
+			name: event.target.files[0].name
+		})
+		//re-render
+		setDatasets([...datasets])
+		console.log(event.target.files[0])
+	};
+
 	return (
 		<div className='page_container'>
 			<h1>My Datasets</h1>
 			<div className='options_container'>
-				<button className='btn'>Upload CSV</button>
+				<button className='btn'>
+					Upload CSV
+					<input
+						// hidden
+						type="file"
+						accept=".csv"
+						onChange={changeHandler}
+						style={{ display: "block", margin: "10px auto" }}
+					/>	
+				</button>
 			</div>
 			<div className='datasets_container'>
 				{datasets.map((dataset) => (
